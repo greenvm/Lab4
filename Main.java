3ad19454ea0921b914af8f526b9eeffd8c4c6f0f
greenvm <89651504+greenvm@users.noreply.github.com>
2021-09-21 04:08:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*
+Lab4 
+Victoria Green
+Date: 9/21/21
+*/
+
+import java.util.Random;
+
+class Main {
+  Random r = new Random();
+  public static void main(String[] args) {
+  
+  int randomNum = r.nextInt(50) + 1; 
+  
+  System.out.prtinln(" ")
+  }
+}
