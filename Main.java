47dd13c9a7353ce2ede465b34a98318388a59db2
greenvm <89651504+greenvm@users.noreply.github.com>
2021-09-21 05:07:54 +0000

    add in while loop and if statements

--- Main.java
+++ Main.java
@@ -7,11 +7,27 @@ Date: 9/21/21
 import java.util.Random;
 
 class Main {
-  Random r = new Random();
   public static void main(String[] args) {
-  
+  Random r = new Random();
+
   int randomNum = r.nextInt(50) + 1; 
+  System.out.println("The random number is: " + randomNum);
   
-  System.out.prtinln(" ")
+  int randNum = randomNum; 
+  //needed in order to count down and print the correct words
+
+  while (randNum >0){
+    randNum--; 
+    System.out.println(randNum);
   }
+  if (randomNum <=5){
+    System.out.println("Ahoy mateys!");
+  }
+  else if (randomNum >25 && randomNum <42){
+    System.out.println("Cannonball!");
+  }
+  else {
+    System.out.println("Blast Off!");
+  }
+}
 }
